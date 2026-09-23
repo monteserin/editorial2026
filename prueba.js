5f8a5ea316ef225895b03fb18977074712b7fb57
Pablo <paramonty@gmail.com>
2026-09-23 14:26:55 +0200

    subida

--- prueba.js
+++ prueba.js
@@ -1,9 +1,1 @@
-const cosas = ["corazon", "cara sonriente"];
-cosas.push("uyuyuy"); // Mete al final
-console.log(cosas); // ["corazon", "cara sonriente", "uyuyuy"];
-cosas.pop(); // Elimina del final
-console.log(cosas); // ["corazon", "cara sonriente"];
-cosas.unshift("amor"); // Mete al principio
-console.log(cosas); // ["amor", "corazon", "cara sonriente"];
-cosas.shift(); // Elimina del principio
-console.log(cosas); // ["corazon", "cara sonriente"];
+jkjkjk
